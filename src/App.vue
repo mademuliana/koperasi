1aabc1f373ccf46208e79aa87efb76a23be94034
muliana <mulianaakun@gmail.com>
2019-06-20 14:37:46 +0800

    new

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
+  <div >
+    <div id="app"></div>
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img alt="Vue logo" src="./assets/logo.png" />
+    
   </div>
 </template>
 
